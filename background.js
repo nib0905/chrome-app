5fa36767e43aca2ca728d9ffc80693407b289a2e
Hyebin Lee <970905hyebin@gmail.com>
2022-12-05 03:19:47 +0900

    Trigger rebuild

--- background.js
+++ background.js
@@ -31,4 +31,4 @@ const images = [
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url(../img/${chosenImage})`;
+document.body.style.backgroundImage = `url(./img/${chosenImage})`;
